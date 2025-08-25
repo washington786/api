5df5d6651b23389a11f03f0bf5f63faaedcf8779
washington786 <dkmawasha@gmail.com>
2025-08-25 18:15:30 +0200

    User model

---
+++ src/models/Users.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+}, { timestamps: true });
+
+export default model('User', userSchema);

0491d8ac7c5a85f2da084abdd033771644993195
washington786 <dkmawasha@gmail.com>
2025-08-26 14:49:45 +0200

    user password hashing hook in schema

---
+++ src/common/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    role: 'user' | 'admin';
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,10 +1,25 @@
 import { Schema, model } from 'mongoose';
+import type { IUser } from '../common/interfaces/IUser.js';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new Schema({
+const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
 }, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    // If password not modified, skip hashing
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(12);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err: any) {
+        next(err);
+    }
+});
 
 export default model('User', userSchema);

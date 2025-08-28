f9fa7dbe8df5a284ed686c881cf98a13a623b548
washington786 <dkmawasha@gmail.com>
2025-08-28 12:01:07 +0200

    added new cors for access

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import swagger from './middlewares/Swagger.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173' || "https://api-qve5.onrender.com",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
